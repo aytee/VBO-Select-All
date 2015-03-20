c11a0fdda998aeb8e16f0f592e5e62294716fe4f
Rudie Dirkx <drupal@hotblocks.nl>
2015-03-20 23:02:22 +0100

    +multiple forms

--- vbo_multipage_selection.js
+++ vbo_multipage_selection.js
@@ -1,20 +1,34 @@
 (function ($) {
 
+  // Forget previous persistent selection(s).
+  // @todo Make this work with batched VBO. Somehow JS can't delete the cookie if it was set
+  // in a batch (AJAX?) response. Tested Chrome and Firefox. With batching disabled, it works
+  // perfectly.
+  document.cookie.replace(/vbomps_clean_(\w+)/g, function(input, id) {
+    var localStorageKey = 'vbomps_' + id;
+    delete localStorage[localStorageKey];
+    document.cookie = input + '=x; expires=Thu, 01 Jan 1970 00:00:01 GMT';
+    console.log('FORGOT VBO MPS FOR: ' + id);
+  });
+
   Drupal.behaviors.vboMultipageSelection = {
     attach: function(context, settings) {
 
       var $form = $('form.vbo-multipage-selection', context);
       if ($form.length) {
+
+        var id = $form.data('vbo-mps-id');
+        var localStorageKey = 'vbomps_' + id;
         var $summary = $form.find('.vbo-multipage-selection-summary');
         var $remembered = $form.find('#vbo-multipage-selection-ids');
         var inputs = $form.find('input.vbo-select');
 
         // Get remembered ids.
-        var ids = localStorage.vboMultipageSelection ? localStorage.vboMultipageSelection.split(',') : [];
+        var ids = localStorage[localStorageKey] ? localStorage[localStorageKey].split(',') : [];
 
         function showSummary(ids) {
           // Save into hidden input.
-          $remembered.val(localStorage.vboMultipageSelection || '');
+          $remembered.val(localStorage[localStorageKey] || '');
 
           // Show number of selected rows.
           if ($summary.length) {
@@ -36,7 +50,7 @@
         // Listen to current page's checkboxes to persist selections.
         inputs.click(function() {
           var id = this.value;
-          var ids = localStorage.vboMultipageSelection ? localStorage.vboMultipageSelection.split(',') : [];
+          var ids = localStorage[localStorageKey] ? localStorage[localStorageKey].split(',') : [];
           var i = ids.indexOf(id);
 
           // Add to list, if it's not in there already.
@@ -53,15 +67,11 @@
           }
 
           // Save and summarize.
-          localStorage.vboMultipageSelection = ids.join(',');
+          localStorage[localStorageKey] = ids.join(',');
           showSummary(ids);
         });
 
-        // Forget selection when a VBO action is triggered.
-        $form.find('.vbo-action-button').click(function() {
-          localStorage.vboMultipageSelection = '';
-        });
-      }
+      } // if $form
 
     }
   };
